6a02d29b9eaed9bc5097bbae01f4240a03fa57e7
Andrey <natsaievv@gmail.com>
2024-11-11 11:57:03 +0200

    edits

--- task1.js
+++ task1.js
@@ -2,12 +2,14 @@ import fs from 'fs';
 
 fs.mkdir('folder', (err) => {
     if (err) {
-        console.error(`Error creating folder: ${err}`);
-} console.log('Folder created successfully'); 
+        return console.error(`Error creating folder: ${err}`);
+    }
+    console.log('Folder created successfully');
 
-fs.rmdir('folder', (err) => {
-    if (err) {
-        console.error(`Error deleting folder: ${err}`);
-} console.log('Folder deleted successfully'); 
-})
-})
+    fs.rmdir('folder', (err) => {
+        if (err) {
+            return console.error(`Error deleting folder: ${err}`);
+        }
+        console.log('Folder deleted successfully');
+    });
+});
--- task2.js
+++ task2.js
@@ -3,13 +3,15 @@ import fs from "fs";
 fs.writeFile("hello.txt", "Hello, World!", (err) => {
     if (err) {
         console.error(`Error creating file: ${err}`);
+        return;
     }
     console.log("File created successfully");
-});
 
-fs.readFile("hello.txt", "utf8", (err, data) => {
-    if (err) {
-        console.error(`Error reading file: ${err}`);
-    }
-    console.log(data);
+    fs.readFile("hello.txt", "utf8", (err, data) => {
+        if (err) {
+            console.error(`Error reading file: ${err}`);
+            return;
+        }
+        console.log(data);
+    });
 })
