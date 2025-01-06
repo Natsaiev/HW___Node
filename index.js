1792cfc443116be7de1c809352d638ee83cfeb72
Andrey <natsaievv@gmail.com>
2025-01-06 20:51:20 +0200

    first

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+let products = [
+  { id: 1, name: "Product One", price: 25.99 },
+  { id: 2, name: "Product Two", price: 7.99 },
+  { id: 3, name: "Product Three", price: 59.99 },
+  { id: 4, name: "Product Four", price: 79.99 },
+  { id: 5, name: "Product Five", price: 459.99 },
+  { id: 6, name: "Product Six", price: 9.99 },
+  { id: 7, name: "Product Seven", price: 79.99 },
+  { id: 8, name: "Product Eight", price: 19.99 },
+  { id: 9, name: "Product Nine", price: 34.99 },
+  { id: 10, name: "Product Ten", price: 55.99 },
+];
+app.get("/products", (_req, res) => {
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const product = products.find((p) => p.id === parseInt(req.params.id));
+  if (!product) return res.status(404).json({ message: "Продукт не найден" });
+  res.json(product);
+});
+
+app.post("/products", (req, res) => {
+  const newProduct = {
+    id: products.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  products.push(newProduct);
+  res
+    .status(201)
+    .json({ message: "Продукт успешно создан", product: newProduct });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
